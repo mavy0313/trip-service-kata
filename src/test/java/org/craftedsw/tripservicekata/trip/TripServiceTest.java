6ad43760e051add4f4be0ef397640754d9b409a4
Maksim Vypov <maksim_vypov@epam.com>
2020-03-11 11:08:21 +0400

    Overriden getLoggedInUser

--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,25 @@
 package org.craftedsw.tripservicekata.trip;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.jupiter.api.Test;
+
 public class TripServiceTest {
-	
+
+  @Test
+  void should_throw_an_exception_when_user_is_not_logged_in() {
+    TripService tripService = new TestableTripService();
+
+    assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(null));
+  }
+
+  private class TestableTripService extends TripService {
+
+    @Override
+    protected User getLoggedInUser() {
+      return null;
+    }
+  }
 }
