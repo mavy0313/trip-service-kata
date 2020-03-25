37ac51958e713264550c41d4eaf1198d989dbda4
Maksim Vypov <maksim_vypov@epam.com>
2020-03-26 01:06:07 +0400

    isFriendWith(user) method added to User

--- src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
+++ src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
@@ -1,5 +1,31 @@
 package org.craftedsw.tripservicekata.user;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.craftedsw.tripservicekata.trip.TripServiceTest.UserBuilder;
+import org.junit.jupiter.api.Test;
+
 public class UserTest {
 
+  private static final User BOB = new User();
+  private static final User PAUL = new User();
+
+  @Test
+  void should_inform_when_users_are_not_friends() {
+    User user = UserBuilder.aUser()
+        .friendsWith(BOB)
+        .build();
+
+    assertThat(user.isFriendWith(PAUL), is(false));
+  }
+
+  @Test
+  void should_inform_when_users_are_friends() {
+    User user = UserBuilder.aUser()
+        .friendsWith(BOB, PAUL)
+        .build();
+
+    assertThat(user.isFriendWith(PAUL), is(true));
+  }
 }
